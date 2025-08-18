76dcc5a6ca8a0a7bc4f077b38ba72778b4b651fe
Olamide <basitoderinu@gmail.com>
2025-08-18 04:24:20 +0100

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for Render

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,7 @@ SECRET_KEY = 'django-insecure-=rzp(cro+e3a#mk9(dmmh+)fbb=j4uomikoi_40=r5*2g=f0)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
