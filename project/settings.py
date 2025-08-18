afa99c67e51b9dac9f1f22e5fbd25efcf169682f
Olamide <basitoderinu@gmail.com>
2025-08-18 03:59:31 +0100

    Fix DATABASES config with dj-database-url

--- project/settings.py
+++ project/settings.py
@@ -180,7 +180,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default='django.db.backends.sqlite3', conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3", conn_max_age=600, ssl_require=True)
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
